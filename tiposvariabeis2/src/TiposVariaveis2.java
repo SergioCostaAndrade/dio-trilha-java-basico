880e41fdaf1191e72d715b08306959147e6aece5
SergioCostaAndrade <scandrade1968@gmail.com>
2024-05-01 19:51:10 -0300

    Novo commit

---
+++ tiposvariabeis2/src/TiposVariaveis2.java
@@ -0,0 +1,16 @@
+public class TiposVariaveis2 {
+    public static void main(String[] args) {
+        System.out.println("Aqui");
+        double salarioMinimo = 2500.33;
+        System.out.println(salarioMinimo);
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+        System.out.println(numeroCurto2);
+        int numero = 1;
+        numero = 5;
+        System.out.println(numero);
+        final double VALOR_DE_PI = 3.1415;
+        System.out.println(VALOR_DE_PI);
+    }
+}
